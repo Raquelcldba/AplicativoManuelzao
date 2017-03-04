962df9e43daba303547dfb4e32d0369a98b0a98b
luizrodolfos <luizrodolfos@gmail.com>
2017-03-04 17:20:32 -0300

    Adição de permissões android
    Adição de permissões android

--- manuelzao-v2/mlz/index.android.js
+++ manuelzao-v2/mlz/index.android.js
@@ -1,53 +1,70 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { AppRegistry } from 'react-native';
+import { Container, Header, Title, Content, Body, Spinner } from 'native-base';
+import { Router, Scene, Actions,Switch} from 'react-native-router-flux';
+import firebase from 'firebase';
+
+
+import Login from './src/components/login/login';
+import Sobre from './src/components/about/sobre';
+import Home from './src/components/home/home';
+import Galeria from './src/components/galeria/galeria';
+import CameraMLZ from './src/components/camera/camera';
+
 
 export default class mlz extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
+    constructor(props){
+        super(props);
+        this.state = {
+          loggedIn: false,
+          loaded: false
+        };
+    }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+    componentWillMount() {
+        firebase.auth().onAuthStateChanged(function(user) {
+            if (user) {
+                 this.setState({ loggedIn: true, loaded: true });
+
+            } else { 
+                  this.setState({ loggedIn: false, loaded: true });                
+
+            }
+        }.bind(this));
+    }
+
+    render() {    
+        if (!this.state.loaded) return (<Spinner  style={{marginTop: 30}} color='#d6d6d6' /> );
+
+        if(this.state.loggedIn) { 
+            return (
+                <Container style = {{ backgroundColor: '#f2f2f2' } }>
+                    <Router>
+                        <Scene key = "root">            
+                            <Scene key = 'home' component = { Home } title = 'Home' hideNavBar = { true } initial={true}/> 
+                            <Scene key = 'CameraMLZ' component = { CameraMLZ } title = 'cameraMLZ' hideNavBar = { true }  />
+                             <Scene key = 'Galeria' component = { Galeria } title = 'Galeria'  />
+                            <Scene key = 'login' component = { Login } title = 'Login' hideNavBar / >             
+                        </Scene>  
+                    </Router>
+                </Container>
+            )            
+        } else {
+             return ( 
+                <Container style = {{ backgroundColor: '#f2f2f2' } }>
+                    <Router>
+                        <Scene key = "root"  >   
+                            <Scene key = 'login' component = { Login } title = 'Login' hideNavBar initial={true}/ >
+                            <Scene key = 'home' component = { Home } title = 'Home' hideNavBar = { true }/>  
+                            <Scene key = 'sobre' component = { Sobre } title = 'Sobre' hideNavBar = { false } />                             
+                            <Scene key = 'CameraMLZ' component = { CameraMLZ } title = 'cameraMLZ' hideNavBar = { true }  />
+                            <Scene key = 'Galeria' component = { Galeria } title = 'Galeria'  />                      
+                        </Scene>  
+                    </Router>
+                </Container>
+            );             
+        }
+    }
+}
 
 AppRegistry.registerComponent('mlz', () => mlz);
--- mlz/index.android.js
+++ mlz/index.android.js
@@ -1,53 +1,38 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import {AppRegistry } from 'react-native';
+import { Container, Header, Title, Content, Body} from 'native-base';
+import { Router, Scene, Actions } from 'react-native-router-flux';
+import firebase from 'firebase';
+
+
+import Login from './src/components/login/login';
+import Sobre from './src/components/about/sobre';
+import Home from './src/components/home/home';
+
+
 
 export default class mlz extends Component {
+componentWillMount() {
+    firebase.auth().onAuthStateChanged(function(user) {
+     if (user) {   
+         Actions.home();    
+      } else {      
+      }
+    });
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+    <Container  style={{ backgroundColor: '#f2f2f2'}}> 
+        <Router> 
+           <Scene key="root">     
+                <Scene key='login' component={Login} title='Login' hideNavBar />
+                <Scene key='sobre' component={Sobre} title='Sobre' hideNavBar={false} /> 
+                <Scene key='home' component={Home} title='Home' hideNavBar={true} /> 
+            </Scene> 
+        </Router>
+    </Container>
     );
-  }
+    }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
 AppRegistry.registerComponent('mlz', () => mlz);
